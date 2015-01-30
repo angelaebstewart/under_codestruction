c5c0d6ac7cf8da095739e4d751fd5a7668855ee6
wjc0008 <wjc0008@auburn.edu>
2015-01-29 19:41:03 -0600

    Initial Commit

---
+++ public/index.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * A simple, clean and secure login / user authentication solution embedded into a small PHP framework.
+ * Part of a set of login scripts, more on http://www.php-login.net
+ *
+ * PHP LOGIN (FRAMEWORK VERSION) 2.1 (in development)
+ *
+ * @author Panique
+ * @link http://www.php-login.net/
+ * @link https://github.com/panique/php-login/
+ * @license http://opensource.org/licenses/MIT MIT License
+ */
+
+require '../vendor/autoload.php';
+// Start our application
+new Application();
