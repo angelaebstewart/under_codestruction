36cb157be72b32d6fb3a5838efdb5ba148ed30c5
wjc0008 <wjc0008@auburn.edu>
2015-01-31 22:36:22 -0600

    Applied a fix to the configuration class, Since it was using a static variable to store the array and only loads the array when it needs to be load, so this fixes it temporary, what will need to take place is a more clever method of maybe checking if the array to the config is already loaded and if it is then do not load it again... check back on this.

--- application/core/Config.php
+++ application/core/Config.php
@@ -1,18 +1,25 @@
 <?php
 
+/*
+ * The Config will reload the array each time a configuration needs to be obtained
+ * This is not great performace for the long run, need to think of a way to do 
+ * this more efficiently.  
+ */
 class Config {
-
-    private static $config;
-
     public static function get($key, $configType = null) {
         if (isset($configType)) {
             $fileName = '../application/config/' . $configType . '.config.php';
 
             if (file_exists($fileName)) {
-                if (!self::$config) {
-                    self::$config = require($fileName);
+                $config = require($fileName);
+                try{
+                $result = $config[$key];
+                //Will this catch an undefined index exception?
+                } catch(ErrorException $e){
+                    echo $e->getMessage();
+                    return null;
                 }
-                return self::$config[$key];
+                return $result;
             }
         }
     }
