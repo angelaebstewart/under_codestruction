e20cd195829726b270895aaa0bd906c653c965c3
Angela Stewart <Angela Stewart@Angela>
2015-02-02 20:42:40 -0600

    Added some dummy code for the lesson 1 game.

--- application/view/lesson/viewGame.php
+++ application/view/lesson/viewGame.php
@@ -1,8 +1,27 @@
 <div class="container">
 
     <h4>Game for Lesson <?php echo Request::get('id'); ?></h4>
-
-    <p>(Game will go here)</p>
+    <script language ="JavaScript">
+        function checkAnswer (form) {
+            if (form.B.checked == true) {
+                alert("Correct answer!");
+             }
+                
+            else {
+                alert("Incorrect answer!");
+            }
+        }
+        
+    </script>
+    
+    <p>Select the correct statement for the loop body so that the loop terminates.
+    <br>x = 0 <br> while (x < 10) <br> _____________</p>
+    <form name ="GameQ1" method="POST" action="viewLesson.php">
+        <input type="radio" name="q1" value="A" id="A"/>x--<br>
+        <input type="radio" name="q1" value="B" id ="B"/>x++<br>
+        <input type="radio" name="q1" value="C" id="C"/>x = -22<br>
+        <p><input type="submit" value="Submit Answer" onClick="checkAnswer(this.form)"/></p>
+    </form>
 
     <a href="<?php echo Config::get('URL','gen'); ?>lesson/viewLesson/?id=<?php echo Request::get('id'); ?>"><- Back to lesson page</a>
 </div> <!-- /container -->
