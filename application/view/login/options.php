66fc9455bab74305422794e0be02362cf4b727ec
ryan-c-lewis <ryan@172.17.16.92>
2015-02-26 13:18:31 -0600

    Getting rid of that "Edit username" page under "Account->Options." Replaced it with a blank "Account options" page.

---
+++ application/view/login/options.php
@@ -0,0 +1,10 @@
+<div id="contentArea">
+    <div class="contentDivision"> 
+        <h1>Account Options</h1>
+        
+        (Coming soon)
+
+        <!-- echo out the system feedback (error and success messages) -->
+        <?php $this->renderFeedbackMessages(); ?>
+    </div>
+</div>
