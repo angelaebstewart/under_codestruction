af0e86e59428319cd7208d02b80572afbc7adb15
ethan <mata.ethan@gmail.com>
2015-04-20 13:02:55 -0500

    Fix for unicode characters being used for shorter passwords

--- application/controller/AccountController.php
+++ application/controller/AccountController.php
@@ -89,7 +89,7 @@ class AccountController extends Controller {
         $passwordNew = Request::post('password1');
         $passwordRetyped = Request::post('password2');
         if (isset($user_id) && isset($passwordNew) && isset($passwordRetyped)) {
-            if (($passwordNew == $passwordRetyped) && (strlen($passwordNew) >= 6 && strlen($passwordRetyped) >= 6)) {
+            if (($passwordNew == $passwordRetyped) && mb_strlen($passwordNew) >= 6) {
                 ChangePasswordModel::setNewPassword($user_id, $passwordNew, $passwordRetyped);
                 Redirect::to('login/index');
             } else {
